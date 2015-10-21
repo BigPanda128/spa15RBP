692c6e4b1c5b3dd84658c33c83afe1b29fe99680
BigPanda128 <rpw10104@saintjoe.edu>
2015-10-20 23:48:49 -0500

    app.js changes according to 7.3.1.1

--- ch07/7.2.8.1-express-static/app.js
+++ ch07/7.2.8.1-express-static/app.js
@@ -15,34 +15,68 @@
 var
   http    = require( 'http'    ),
   express = require( 'express' ),
+  morgan = require( 'morgan' ),
+  bodyParser = require( 'body-parser' ),
+  methodOverride = require( 'method-override' ),
+  errorHandler = require( 'errorhandler' ),
 
   app     = express(),
   server  = http.createServer( app );
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 // ------------- BEGIN SERVER CONFIGURATION ---------------
-app.configure( function () {
-  app.use( express.bodyParser() );
-  app.use( express.methodOverride() );
+app.use( bodyParser.urlencoded() );
+  app.use( methodOverride() );
   app.use( express.static( __dirname + '/public' ) );
-  app.use( app.router );
-});
 
-app.configure( 'development', function () {
-  app.use( express.logger() );
-  app.use( express.errorHandler({
-    dumpExceptions : true,
-    showStack      : true
-  }) );
-});
+  app.use( morgan( 'common' ) );
+  app.use( errorHandler() );
+//  app.use( errorHandler({
+//  dumpExceptions : true,
+//    showStack      : true
+// });
 
-app.configure( 'production', function () {
-  app.use( express.errorHandler() );
-});
-
+// all configurations below are for routes
 app.get( '/', function ( request, response ) {
   response.redirect( '/spa.html' );
 });
+
+app.all( '/user/*?', function ( request, response, next ) {
+  response.contentType( 'json' );
+  next();
+});
+
+app.get( '/user/list', function ( request, response ) {
+  response.send({ title: 'user list' });
+});
+
+app.post( '/user/create', function ( request, response ) {
+  response.send({ title: 'user created' });
+});
+
+app.get( '/user/read/:id([0-9]+)',
+  function ( request, response ) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' found'
+    });
+  }
+);
+
+app.post( '/user/update/:id([0-9]+)',
+  function ( request, response ) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' updated'
+    });
+  }
+);
+
+app.get( '/user/delete/:id([0-9]+)',
+  function ( request, response ) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' deleted'
+    });
+  }
+);
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
